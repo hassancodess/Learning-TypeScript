afa47e227d481a4cbf4f88fd14099cdeb80aa1e8
hassancodess <dev.hassancodes@gmail.com>
2022-08-13 03:08:13 +0500

    Add Types

---
+++ 01-Types/variables.js
@@ -0,0 +1,8 @@
+var myName = "Hassan";
+myName.toUpperCase(); // HASSAN
+myName.toLowerCase(); // hassan
+myName = "9";
+var myNumber = 9;
+myNumber = 4;
+var myBool = true;
+myBool = false;
---
+++ 01-Types/variables.ts
@@ -0,0 +1,11 @@
+let myName:string = "Hassan"
+myName.toUpperCase() // HASSAN
+myName.toLowerCase() // hassan
+myName  = "9"
+
+let myNumber: number = 9
+myNumber = 4
+
+let myBool: boolean = true
+myBool = false
+
